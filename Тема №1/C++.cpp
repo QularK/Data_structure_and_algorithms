2613431de0b212b662773d6ec53fa18987125c86
QularK <fastdenis2007@gmail.com>
2025-10-05 15:13:16 +0300

    Add files via upload

---
+++ Тема №1/C++.cpp
@@ -0,0 +1,25 @@
+//Создание списка
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+	vector<int> number={1,2,3,4,5}
+}
+
+//Создание стека 
+
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+	stack<int> my Stack;
+	myStack.push(1)
+	myStack.push(2)
+	myStack.push(3)
+	myStack.push(4)
+	myStack.push(5)
+}
---
+++ Тема №1/Java.java
@@ -0,0 +1,17 @@
+//Создание списка 
+
+import java.util.List;
+import java.util.Arrays;
+
+final List<Integer> list=Arrays.asList(1,2,3,4,5)
+
+
+//Создание стека
+
+Stack<Type> stacks = new stack<>()
+
+Stack.push(1)
+Stack.push(2)
+Stack.push(3)
+Stack.push(4)
+Stack.push(5)
